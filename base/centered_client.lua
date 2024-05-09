621f3f6e1d4d1a0449df22a39022a1c9a08f833b
Gabriel Guerra <gfranklings@gmail.com>
2024-05-09 16:58:28 -0400

    feat: centering clients when they open centered

---
+++ base/centered_client.lua
@@ -0,0 +1,7 @@
+local awful = require("awful")
+
+Client.connect_signal("request::manage", function (client)
+    if client.floating or awful.layout.get(client.tag) == awful.layout.suit.floating then
+        awful.placement.centered(client)
+    end
+end)
